b901aef45ae81b4461ee355077f6407d8c122481
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-03-14 17:40:41 +0100

    Completed assigned task

---
+++ Lab1/Assigned.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+/*
+Wyświetl zawartość żądanej przez użytkownika
+linii z pliku `dane.txt`.
+Numer linii podany jest w linku jako parametr `lineNumber`.
+Linie numerowane są od 0.
+
+*/
+
+func main() {
+	showLines([]int{1, 2, 3})
+}
+
+func showLine(line int) string {
+	data, err := os.ReadFile("data.txt")
+	if err != nil {
+		return "File corrupted"
+	}
+	dataList := strings.Fields(string(data))
+	return dataList[line-1]
+}
+
+func showLines(lines []int) {
+	for _, i := range lines {
+		fmt.Printf("Line %d: %s", i, showLine(i))
+		fmt.Println()
+	}
+}
