31abead065ce9e189274acb2903ec13e11ebd26c
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-04-04 14:56:12 +0200

    Lab4 phase 1 completed

--- Lab4/Guess.go
+++ Lab4/Guess.go
@@ -1,5 +1,47 @@
-package Lab4
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 func main() {
+	game()
+}
 
+func game() {
+	fmt.Printf("Welcome to THE GAME!\nYou have to guess number between 1 and 500.\n")
+	numberToBeGuessed := randomNumber(1, 500)
+	var guess string
+	tries := 1
+	for {
+		fmt.Printf("Enter number:\t")
+		fmt.Scanf("%s\n", &guess)
+		intGuess, _ := strconv.Atoi(guess)
+		if checkGuess(numberToBeGuessed, intGuess) {
+			break
+		}
+		tries++
+	}
+	fmt.Printf("You've made in in %d tries", tries)
+}
+
+func randomNumber(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
+}
+
+func checkGuess(number, guess int) bool {
+	if number == guess {
+		fmt.Printf("Congratulations!\nYou've guessed the number \"%d\"\n", number)
+		return true
+	} else if guess > number {
+		fmt.Printf("No, %d is too big!\n", guess)
+		return false
+	} else {
+		fmt.Printf("No, %d is too small!\n", guess)
+		return false
+	}
 }
