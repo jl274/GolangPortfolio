aa23920e57ca3bd314b1d8091e0ae32c95305a0f
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-04-11 15:03:31 +0200

    Rando number generator

--- Lab5/terrarium.go
+++ Lab5/terrarium.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("ok")
 }
 
+// Random number -------------------------------------------
+func randomNumber(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
+}
+
+// Position ------------------------------------------------
 type Position struct {
 	x int
 	y int
