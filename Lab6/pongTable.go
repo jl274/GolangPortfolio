c85cbdb37e081446c1012388fe497e6dcef2459f
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-04-25 20:04:22 +0200

    Serving and getting ready

--- Lab6/pongTable.go
+++ Lab6/pongTable.go
@@ -11,6 +11,9 @@ func main() {
 	racket2 := NewRacket("Player2", "Pong!")
 	racket1.setOpponent(racket2)
 	racket2.setOpponent(racket1)
+	racket2.getReady()
+	racket1.serve()
+	time.Sleep(20000 * time.Millisecond)
 }
 
 // Ball message passed between two players
@@ -20,13 +23,14 @@ type Ball string
 type Racket struct {
 	name     string
 	motto    string
+	playing  bool
 	receive  *chan Ball
 	opponent *Racket
 }
 
 func NewRacket(name, motto string) *Racket {
 	receive := make(chan Ball, 1)
-	racket := Racket{name, motto, &receive, nil}
+	racket := Racket{name, motto, false, &receive, nil}
 	return &racket
 }
 
@@ -35,9 +39,22 @@ func (thisRacket *Racket) setOpponent(opponent *Racket) {
 }
 
 func (thisRacket *Racket) passTheBall() {
-	fmt.Printf("%s: %s\n", thisRacket.name, thisRacket.motto)
-	time.Sleep(1000)
-	*thisRacket.opponent.receive <- Ball("")
-	<-*thisRacket.receive
-	thisRacket.passTheBall()
+	for {
+		fmt.Printf("%s: %s\n", thisRacket.name, thisRacket.motto)
+		time.Sleep(1000 * time.Millisecond)
+		*thisRacket.opponent.receive <- Ball("")
+		<-*thisRacket.receive
+	}
+
+}
+
+func (thisRacket *Racket) getReady() {
+	go func() {
+		<-*thisRacket.receive
+		go thisRacket.passTheBall()
+	}()
+}
+
+func (thisRacket *Racket) serve() {
+	go thisRacket.passTheBall()
 }
