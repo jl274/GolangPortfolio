b9246a1bfcc7c19426ce5659b2271e538f93401f
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-04-26 18:17:30 +0200

    Counter constructors

--- Lab6/wordCounter.go
+++ Lab6/wordCounter.go
@@ -9,10 +9,20 @@ func main() {
 // Placeholder for map with counted words
 type counterMap map[string]int
 
-// Counter holds total record of words in file, and receives new information from Readers
+// Counter holds total record of words in file, and receives new information from Reader
 type Counter struct {
 	wordsMap counterMap
 	wordsBox chan counterMap
+}
+
+func (c *Counter) NewCounter() Counter {
+	wMap := map[string]int{}
+	wChan := make(chan counterMap)
+	return Counter{wMap, wChan}
+}
+
+func (c *Counter) MakeReader(line string) Reader {
+	return Reader{line, c}
 }
 
 // Reader receives lines, counts word in it and sends it back to Counter
