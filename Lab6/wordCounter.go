add4cbddc1417e981c3d51d78561fdbf70308156
Jakub Lendzinski <j.lendzinski.390@studms.edu.ug.pl>
2022-04-26 19:08:42 +0200

    Concurrency loop

--- Lab6/wordCounter.go
+++ Lab6/wordCounter.go
@@ -17,32 +17,67 @@ type counterMap map[string]int
 // Counter holds total record of words in file, and receives new information from Reader
 type Counter struct {
 	wordsMap counterMap
+	received int
 	wordsBox chan counterMap
 }
 
 func (c *Counter) NewCounter() Counter {
 	wMap := map[string]int{}
 	wChan := make(chan counterMap)
-	return Counter{wMap, wChan}
+	return Counter{wMap, 0, wChan}
 }
 
-func (c *Counter) MakeReader(line string) Reader {
-	return Reader{line, c}
+func (c *Counter) MakeReader() Reader {
+	return Reader{c, false}
 }
 
 // Reader receives lines, counts word in it and sends it back to Counter
 type Reader struct {
-	line       string
 	supervisor *Counter
+	working    bool
+}
+
+func (r *Reader) read(line string) {
+	// working
 }
 
 // File to string array
-func (c *Counter) readFile() []string {
-	absPath, _ := filepath.Abs("results.csv")
+func (c *Counter) readFile(file string) []string {
+	absPath, _ := filepath.Abs(file)
 	f, err := os.ReadFile(absPath)
 	if err != nil {
 		err.Error()
 	}
 
 	return strings.Split(string(f), "\n")
+}
+
+// Concurrency loop
+func (c *Counter) count(file string) counterMap {
+	wordsArray := c.readFile(file)
+	workers := make([]Reader, 0, len(wordsArray))
+	for i := 0; i < len(wordsArray); i++ {
+		workers = append(workers, c.MakeReader())
+	}
+	for {
+		for _, worker := range workers {
+			if !worker.working {
+				worker.working = true
+				worker.read(wordsArray[0])
+				wordsArray = wordsArray[1:]
+			}
+		}
+		if c.received == len(wordsArray) {
+			break
+		}
+		select {
+		case temp := <-c.wordsBox:
+			for k, v := range temp {
+				count := c.wordsMap[k] + v
+				c.wordsMap[k] = count
+			}
+			c.received++
+		}
+	}
+	return c.wordsMap
 }
